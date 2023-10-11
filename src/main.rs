28623098725c266b3f9959971948f207114f5347
Michael West <quagly@gmail.com>
2023-10-11 13:03:33 -0700

    add log of panics

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use anyhow::{Context, Result};
 use log::{debug, error, info, trace, warn};
+use log_panics;
 use log4rs;
+// this is just an example of how a module uses the log
+// initialized here
 use rust_cli_example::logmod;
 
 // TODO log panics.  Probably use log_panics crate
@@ -11,8 +14,10 @@ const LOGGING_CONFIG_FILE: &str = "logging-config.yml";
 
 fn main() -> Result<()> {
     log4rs::init_file(LOGGING_CONFIG_FILE, Default::default()).unwrap();
-    // log4rs::init_file("logging-confi.yml", Default::default())
-    //    .with_context(|| format!("Failed to initialize logging from config file {}", file_name))?;
+    // backtrace mode options https://docs.rs/log-panics/latest/log_panics/enum.BacktraceMode.html#variants
+    log_panics::Config::new()
+        .backtrace_mode(log_panics::BacktraceMode::Resolved)
+        .install_panic_hook();
     trace!("detailed tracing info");
     debug!("debug info");
     info!("relevant general info");
@@ -22,6 +27,8 @@ fn main() -> Result<()> {
         info!("To test rolling file configurations we print this message in a loop. This is loop nr. {}", i);
     }
     logmod::run();
+    // demonstrate log_panics
+    // panic!("very bad!");
     
     Ok(())
 }
