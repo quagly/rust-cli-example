99b4f8c321ec61bb8c94f0f87f7eb2123c349e55
Michael West <quagly@gmail.com>
2023-10-11 06:16:26 -0700

    made runnable again

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,16 @@ use log::{debug, error, info, trace, warn};
 use log4rs;
 use rust_cli_example::logmod;
 
+// TODO log panics.  Probably use log_panics crate
+// https://docs.rs/log-panics/latest/log_panics/#structs
+// should this be a path instead of string slice?
+// lets make a config struct with defaults for bootstrap settings
+const LOGGING_CONFIG_FILE: &str = "logging-config.yml"; 
+
 fn main() -> Result<()> {
-    log4rs::init_file("logging-config.yml", Default::default()).unwrap();
+    log4rs::init_file(LOGGING_CONFIG_FILE, Default::default()).unwrap();
+    // log4rs::init_file("logging-confi.yml", Default::default())
+    //    .with_context(|| format!("Failed to initialize logging from config file {}", file_name))?;
     trace!("detailed tracing info");
     debug!("debug info");
     info!("relevant general info");
